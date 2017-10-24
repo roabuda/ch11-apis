397571359e6cd57f6092e35b02c4824821e2bae4
Michael Freeman <michael.k.freeman@gmail.com>
2017-10-24 11:29:01 -0700

    Move ex-5 to ex-2, update instructions for API key

--- exercise-2/README.md
+++ exercise-2/README.md
@@ -1,4 +1,6 @@
 # Exercise-2
-In this exercise, you'll practice working with more complex data from the Spotify API. To better understand these endpoints, see <https://developer.spotify.com/web-api/endpoint-reference/>.
+In this exercise, you'll practice using the [New York Times API](https://developer.nytimes.com/) to fetch movie reviews. To learn more about the API, see the [developer console](https://developer.nytimes.com/movie_reviews_v2.json). You'll also need to **register for an API key** that can access there movie reviews API by following [this link](https://developer.nytimes.com/signup).
+
+You should create a file called `api-key.R`, and read it into your `exercise.R` script using the `source()` function. Make sure to add `api-key.R` to your `.gitignore` file (at the root of the chapter repository) so that you **keep your key private**. 
 
 As in previous exercises, you should fork and clone this repository, follow the instructions in `exercise.R` file, then `add` and `commit` your changes so that you can `push` them back up to Github.
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,72 +1,42 @@
-### Exercise 2 ###
+### Exercise 5 ###
+library(jsonlite)
+library(dplyr)
+library(httr)
 
-# Load the httr and jsonlite libraries for accessing data
-library("httr")
-library("jsonlite")
+# Read in API key from api-key.R file (you'll need to create this)
+source('api-key.R')
 
-# Create a `base.uri` variable that holds the base uri. You wil then paste endpoints to this base.
-base.uri <- "https://api.spotify.com"
+# Write a function that allows you to specify a movie, then does the following:
+GetReview <- function(movie) {
+  
+  # Construct a search query using YOUR api key
+  # The base URL is https://api.nytimes.com/svc/movies/v2/reviews/search.json
+  # Your parameters should include a "query" and an "api_key"
+  # Note: the HTTR library will take care of spaces in the arguments
+  # See the interactive console for more detail:https://developer.nytimes.com/movie_reviews_v2.json#/Console/GET/reviews/search.json
+  base.url <- 'https://api.nytimes.com/svc/movies/v2/reviews/search.json'
+  query.params <- list(query=movie, api_key = api.key)
+  response <- GET(base.url, query = query.params)
+  body <- content(response, "text")
+  
+  # Request data using your search query
+  results <- fromJSON(body)
+  
+  # What type of variable does this return?
+  
+  # Flatten the data stored in the `$results` key of the data returned to you
+  flattened <- flatten(results$results)
+  
+  # From the most recent review, store the headline, short summary, and link to full article each in their own variables
+  first.review <- flattened[1,]
+  headline <- first.review$headline
+  summary <- first.review$summary_short
+  link <- first.review$link.url
 
-
-## As you may have noticed, it often takes multiple queries to retrieve the desired information.
-## This is a perfect situation in which writing a function will allow you to better structure your
-## code, and give a name to a repeated task!
-
-
-# Define a function `TopTrackSearch` that takes in an artist name as an argument,
-# and returns the top 10 tracks (in the US) by that artist
-TopTrackSearch <- function(artist) {
-  # Artist info
-  resource <- "/v1/search"
-  uri = paste0(base.uri, resource)
-  query.params <- list(q = artist, type = "artist")
-
-  response <- GET(uri, query = query.params)
-
-  artist.info <- fromJSON(content(response, "text"))
-  artist.id <- artist.info$artists$items$id[1]
-
-  # Get albums
-  resource <- paste0("/v1/artists/",artist.id,"/top-tracks")
-  uri = paste0(base.uri, resource)
-  query.params <- list(country="US")
-  response <- GET(uri, query = query.params)
-
-  top.tracks <- fromJSON(content(response, "text"))
-
-  return(top.tracks$tracks)
+  # Return an list of the three pieces of information from above
+  info <- list(headline=headline, summary=summary, link=link)
+  return(info)
 }
 
-# What are the top 10 tracks by Beyonce?
-top.tracks <- TopTrackSearch('Beyonce')
-
-# Use the `flatten` function to flatten the data.frame -- note what differs!
-top.tracks <- flatten(top.tracks)
-
-# Use the `save()` function to save the flattened data frame to a file `beyonce.Rdata`
-save(top.tracks, file="beyonce.Rdata")
-
-# Use your `dplyr` functions to get the number of the songs that appear on each album
-num.album <- top.tracks %>%
-  group_by(album.name) %>%
-  summarise(n = n()) %>%
-  arrange(-n)
-num.album
-
-
-### Bonus ###
-# Write a function that allows you to specify a search type (artist, album, etc.), and a string,
-# that returns the album/artist/etc. page of interest
-SpotifySearch <- function(type, string) {
-
-  resource <- "/v1/search"
-  uri = paste0(base.uri, resource)
-  query.params <- list(q = string, type = type)
-
-  response <- GET(uri, query = query.params)
-
-  return(flatten(fromJSON(content(response,"text"))))
-}
-
-# Search albums with the word "Sermon"
-SpotifySearch('album', 'Sermon')
+# Test that your function works with a movie of your choice
+AlmostFamousInfo <- GetReview('Almost Famous')
